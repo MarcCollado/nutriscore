6e54d2f2fb1f3b40164e651075354ea27aae4f71
Marc Collado <marc@collado.io>
2021-12-11 19:08:02 +0100

    Add forms for custom API queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,97 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { Formik, Field, Form } from 'formik';
 
 const IndexPage = () => {
   const [data, setData] = useState(null);
 
-  const fetchAPI = () => {
-    fetch(
-      "https://nutri-score-auditor-api.ew.r.appspot.com/calculate?q={%22category%22%20:%20%22others%22,%20%22energy%22%20:%20356.0,%20%22fibre%22%20:%200.0,%20%22fruit_and_vegetables%22%20:%200.0,%20%22is_water%22%20:%20%22False%22,%20%22protein%22%20:%2016.0,%20%22sodium%22%20:%20920.0,%20%22saturated_fats%22%20:%200.0,%20%22saturated_fats_and_lipids%22%20:%200.3,%20%22sugars%22%20:%203.0}"
-    )
-      .then((res) => res.json())
-      .then((res) => setData(res));
-  };
+  const baseUrl = `https://nutri-score-auditor-api.ew.r.appspot.com/`;
 
   return (
     <div>
-      <button onClick={() => fetchAPI()}>Nutri-Score API</button>
+      <Formik
+        initialValues={{
+          category: 'others',
+          // Points A
+          energy: 356,
+          sugars: 3.0,
+          saturatedFats: 0,
+          saturatedFatsAndLipids: 0,
+          sodium: 920,
+          // Points C
+          fruitAndVegetables: 0,
+          fibre: 0,
+          protein: 16,
+          isWater: 'False',
+        }}
+        onSubmit={async (values) => {
+          const foodData = {
+            category: `${values.category}`,
+            energy: values.energy,
+            fibre: values.fibre,
+            fruit_and_vegetables: values.fruitAndVegetables,
+            is_water: `${values.isWater}`,
+            protein: values.protein,
+            sodium: values.sodium,
+            saturated_fats: values.saturatedFats,
+            saturated_fats_and_lipids: values.saturatedFatsAndLipids,
+            sugars: values.sugars,
+          };
+
+          fetch(`${baseUrl}calculate?q=${JSON.stringify(foodData)}`)
+            .then((res) => res.json())
+            .then((res) => setData(res));
+        }}
+      >
+        <Form>
+          <label htmlFor="category">Categoría</label>
+          <Field id="category" name="category" as="select">
+            <option value="queso">Queso</option>
+            <option value="bebidas">Bebidas</option>
+            <option value="grasas">Grasas, aceites, o mantequillas</option>
+            <option value="otros">Otros</option>
+          </Field>
+
+          <h2>Puntos A</h2>
+
+          <label htmlFor="energy">Energía (KJ)</label>
+          <Field id="energy" name="energy" placeholder="0.0" type="number" />
+
+          <label htmlFor="sugars">Azúcares (g)</label>
+          <Field id="sugars" name="sugars" placeholder="0,0" type="number" />
+
+          <label htmlFor="saturatedFats">Grasas saturadas (g)</label>
+          <Field
+            id="saturatedFats"
+            name="saturatedFats"
+            placeholder="0,0"
+            type="number"
+          />
+
+          <label htmlFor="sodium">Sodio (mg)</label>
+          <Field id="sodium" name="sodium" placeholder="0,0" type="number" />
+
+          <h2>Puntos C</h2>
+
+          <label htmlFor="fruitAndVegetables">Frutas y vegetales (%)</label>
+          <Field
+            id="fruitAndVegetables"
+            name="fruitAndVegetables"
+            placeholder="0.0"
+            type="number"
+          />
+
+          <label htmlFor="fibre">Fibra (g)</label>
+          <Field id="fibre" name="fibre" placeholder="0,0" type="number" />
+
+          <label htmlFor="protein">Proteínas (g)</label>
+          <Field id="protein" name="protein" placeholder="0,0" type="number" />
+
+          <button type="submit">Calcular Nutri-score</button>
+        </Form>
+      </Formik>
+
+      <h2>Resultado</h2>
+
       <li>Final score: {data ? data.final_score : null}</li>
       <li>Nutri score: {data ? data.nutri_score : null}</li>
       <li>Points A:</li>
