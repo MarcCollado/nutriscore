9ceadb7603b7a2b427758b74278788e1df911aff
Marc Collado <marc@collado.io>
2021-12-31 11:47:35 +0100

    Add support for retina images

--- src/components/NutriScore.js
+++ src/components/NutriScore.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 const NutriScore = ({ apiResult }) => {
   const letter = apiResult?.nutri_score.toLowerCase();
 
   return (
     apiResult && (
-      <Box
-        component="img"
-        sx={{
-          height: 233,
-          width: 350,
-          maxHeight: { xs: 233, md: 167 },
-          maxWidth: { xs: 350, md: 250 },
-        }}
-        alt="Nutri-Score"
-        src={require(`../assets/img/nutri_score_${letter}.png`)}
-      />
+      <>
+        <Typography sx={{ mt: 4 }} variant="h5">
+          Resultado
+        </Typography>
+        <Box
+          component="img"
+          sx={{
+            mx: 'auto',
+            my: 1,
+            maxWidth: { xs: 200, sm: 250 },
+          }}
+          alt="Nutri-Score"
+          src={require(`../assets/img/nutri_score_${letter}_2x.png`)}
+        />
+      </>
     )
   );
 };
