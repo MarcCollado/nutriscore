83008fbc4c2a6f337192cad6f7f54cb9e93c60f7
Marc Collado <marc@collado.io>
2022-01-02 19:41:21 +0100

    Refactor energy column

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -10,51 +10,101 @@ import {
 } from '@mui/material';
 
 const Detail = ({ apiResult, formData }) => {
-  function createPointsATableRow(beveragesEnergy, beveragesSugars, key, notBeveragesEnergy, notBeveragesSugars, sodium) {
-    const colors = ['#fcece9', '#f9d9d3', '#f6c6bd', '#f4b3a8', '#f1a092', '#ee8d7c', '#ec7a67', '#e96751', '#e6543b', '#e44226']
+  const createPointsATableRow = (
+    beveragesEnergy,
+    beveragesSugars,
+    key,
+    notBeveragesEnergy,
+    notBeveragesSugars,
+    sodium
+  ) => {
+    const colors = [
+      '#ffffff',
+      '#fcece9',
+      '#f9d9d3',
+      '#f6c6bd',
+      '#f4b3a8',
+      '#f1a092',
+      '#ee8d7c',
+      '#ec7a67',
+      '#e96751',
+      '#e6543b',
+      '#e44226',
+    ];
     return (
       <TableRow key={key}>
-        <TableCell align="right" component="th" scope="row" sx={{ backgroundColor: colors[key - 1] }}>{key}</TableCell>
-        {formData.category !== 'beverages' && apiResult.points_a.a === key &&
-            <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>{notBeveragesEnergy}</TableCell>
-        }
-        {formData.category !== 'beverages' && apiResult.points_a.a !== key &&
-            <TableCell align="right">{notBeveragesEnergy}</TableCell>
-        }
-        {formData.category === 'beverages' && apiResult.points_a.a === key &&
-            <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>{beveragesEnergy}</TableCell>
-        }
-        {formData.category === 'beverages' && apiResult.points_a.a !== key &&
-            <TableCell align="right">{beveragesEnergy}</TableCell>
-        }
-        {formData.category !== 'beverages' && apiResult.points_a.b === key &&
-            <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>{notBeveragesSugars}</TableCell>
-        }
-        {formData.category !== 'beverages' && apiResult.points_a.b !== key &&
-            <TableCell align="right">{notBeveragesSugars}</TableCell>
-        }
-        {formData.category === 'beverages' && apiResult.points_a.b === key &&
-            <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>{beveragesSugars}</TableCell>
-        }
-        {formData.category === 'beverages' && apiResult.points_a.b !== key &&
-            <TableCell align="right">{beveragesSugars}</TableCell>
-        }
-        {apiResult.points_a.c === key ?
-            <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>{'> ' + key}</TableCell>
-            : <TableCell align="right">{'> ' + key}</TableCell>
-        }
-        {apiResult.points_a.d === key ?
-            <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>{sodium}</TableCell>
-            : <TableCell align="right">{sodium}</TableCell>
-        }
+        <TableCell
+          align="right"
+          component="th"
+          scope="row"
+          sx={{ backgroundColor: colors[key] }}
+        >
+          {key}
+        </TableCell>
+
+        {/* Energy */}
+        {formData.category !== 'beverages' ? (
+          <TableCell
+            align="right"
+            sx={{
+              backgroundColor:
+                apiResult.points_a.a === key ? 'LightGrey' : '#ffffff',
+            }}
+          >
+            {notBeveragesEnergy}
+          </TableCell>
+        ) : (
+          <TableCell
+            align="right"
+            sx={{
+              backgroundColor:
+                apiResult.points_a.a === key ? 'LightGrey' : '##ffffff',
+            }}
+          >
+            {beveragesEnergy}
+          </TableCell>
+        )}
+
+        {/* @danielgbaena este es pa ti ;) */}
+        {formData.category !== 'beverages' && apiResult.points_a.b === key && (
+          <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>
+            {notBeveragesSugars}
+          </TableCell>
+        )}
+        {formData.category !== 'beverages' && apiResult.points_a.b !== key && (
+          <TableCell align="right">{notBeveragesSugars}</TableCell>
+        )}
+        {formData.category === 'beverages' && apiResult.points_a.b === key && (
+          <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>
+            {beveragesSugars}
+          </TableCell>
+        )}
+        {formData.category === 'beverages' && apiResult.points_a.b !== key && (
+          <TableCell align="right">{beveragesSugars}</TableCell>
+        )}
+        {apiResult.points_a.c === key ? (
+          <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>
+            {'> ' + key}
+          </TableCell>
+        ) : (
+          <TableCell align="right">{'> ' + key}</TableCell>
+        )}
+        {apiResult.points_a.d === key ? (
+          <TableCell align="right" sx={{ backgroundColor: 'LightGrey' }}>
+            {sodium}
+          </TableCell>
+        ) : (
+          <TableCell align="right">{sodium}</TableCell>
+        )}
       </TableRow>
-    )
-  }
+    );
+  };
 
   return (
     apiResult && (
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 400 }} aria-label="Detail table">
+          {/* Table headers */}
           <TableHead>
             <TableRow>
               <TableCell align="right">Puntos</TableCell>
@@ -64,18 +114,97 @@ const Detail = ({ apiResult, formData }) => {
               <TableCell align="right">Sodio (mg)</TableCell>
             </TableRow>
           </TableHead>
+          {/* Table body */}
           <TableBody>
-            {createPointsATableRow('≤ 0,0', '≤ 0,0', 0, '≤ 335', '≤ 4,5', '≤ 90')}
-            {createPointsATableRow('≤ 30', '≤ 3,0', 1, '> 335', '> 4,5', '> 90')}
-            {createPointsATableRow('≤ 60', '> 13,5', 2, '> 670', '> 9,0', '> 180')}
-            {createPointsATableRow('≤ 90', '≤ 4,5', 3, '> 1005', '> 13,5', '> 270')}
-            {createPointsATableRow('≤ 120', '≤ 6,0', 4, '> 1340', '> 18,0', '> 360')}
-            {createPointsATableRow('≤ 150', '≤ 7,5', 5, '> 1675', '> 22,5', '> 450')}
-            {createPointsATableRow('≤ 180', '≤ 9,0', 6, '> 2010', '> 27,0', '> 540')}
-            {createPointsATableRow('≤ 210', '≤ 10,5', 7, '> 2345', '> 31,0', '> 630')}
-            {createPointsATableRow('≤ 240', '≤ 12,0', 8, '> 2680', '> 36,0', '> 720')}
-            {createPointsATableRow('≤ 270', '≤ 13,5', 9, '> 3015', '> 40,0', '> 810')}
-            {createPointsATableRow('> 270', '> 13,5', 10, '> 3350', '> 45,0', '> 900')}
+            {createPointsATableRow(
+              '≤ 0,0',
+              '≤ 0,0',
+              0,
+              '≤ 335',
+              '≤ 4,5',
+              '≤ 90'
+            )}
+            {createPointsATableRow(
+              '≤ 30',
+              '≤ 3,0',
+              1,
+              '> 335',
+              '> 4,5',
+              '> 90'
+            )}
+            {createPointsATableRow(
+              '≤ 60',
+              '> 13,5',
+              2,
+              '> 670',
+              '> 9,0',
+              '> 180'
+            )}
+            {createPointsATableRow(
+              '≤ 90',
+              '≤ 4,5',
+              3,
+              '> 1005',
+              '> 13,5',
+              '> 270'
+            )}
+            {createPointsATableRow(
+              '≤ 120',
+              '≤ 6,0',
+              4,
+              '> 1340',
+              '> 18,0',
+              '> 360'
+            )}
+            {createPointsATableRow(
+              '≤ 150',
+              '≤ 7,5',
+              5,
+              '> 1675',
+              '> 22,5',
+              '> 450'
+            )}
+            {createPointsATableRow(
+              '≤ 180',
+              '≤ 9,0',
+              6,
+              '> 2010',
+              '> 27,0',
+              '> 540'
+            )}
+            {createPointsATableRow(
+              '≤ 210',
+              '≤ 10,5',
+              7,
+              '> 2345',
+              '> 31,0',
+              '> 630'
+            )}
+            {createPointsATableRow(
+              '≤ 240',
+              '≤ 12,0',
+              8,
+              '> 2680',
+              '> 36,0',
+              '> 720'
+            )}
+            {createPointsATableRow(
+              '≤ 270',
+              '≤ 13,5',
+              9,
+              '> 3015',
+              '> 40,0',
+              '> 810'
+            )}
+            {createPointsATableRow(
+              '> 270',
+              '> 13,5',
+              10,
+              '> 3350',
+              '> 45,0',
+              '> 900'
+            )}
+
             {/* Category score */}
             <TableRow key="11" sx={{ border: 2 }}>
               <TableCell align="right" component="th" scope="row"></TableCell>
