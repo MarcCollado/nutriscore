b8d1acdbf52920405f4fc29f09331e90d7973489
Marc Collado <marc@collado.io>
2022-01-04 19:23:18 +0100

    Add colors file

---
+++ src/utils/colors.js
@@ -0,0 +1,38 @@
+export const tableColors = {
+  pointsA: [
+    '#ffffff',
+    '#fcece9',
+    '#f9d9d3',
+    '#f6c6bd',
+    '#f4b3a8',
+    '#f1a092',
+    '#ee8d7c',
+    '#ec7a67',
+    '#e96751',
+    '#e6543b',
+    '#e44226',
+  ],
+  pointsC: {
+    beverages: [
+      '#ffffff',
+      '#e8f2ec',
+      '#d1e5da',
+      '#bad8c7',
+      '#a3cbb5',
+      '#8cbfa2',
+      '#75b290',
+      '#5ea57d',
+      '#47986b',
+      '#308b58',
+      '#197f46',
+    ],
+    notBeverages: [
+      '#ffffff',
+      '#d1e5da',
+      '#a3cbb5',
+      '#75b290',
+      '#47986b',
+      '#197f46',
+    ],
+  },
+};
