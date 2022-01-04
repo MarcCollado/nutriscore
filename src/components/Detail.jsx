10795c87a1ebbb9667c9a765a273192191f34f46
Marc Collado <marc@collado.io>
2022-01-04 19:23:31 +0100

    Refactor table A and C

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -9,6 +9,8 @@ import {
   TableRow,
 } from '@mui/material';
 
+import { tableColors } from '../utils/colors';
+
 const Detail = ({ apiResult, formData }) => {
   const createPointsATableRow = (
     beveragesEnergy,
@@ -18,129 +20,93 @@ const Detail = ({ apiResult, formData }) => {
     notBeveragesEnergy,
     notBeveragesSugars,
     sodium
-  ) => {
-    const colors = [
-      '#ffffff',
-      '#fcece9',
-      '#f9d9d3',
-      '#f6c6bd',
-      '#f4b3a8',
-      '#f1a092',
-      '#ee8d7c',
-      '#ec7a67',
-      '#e96751',
-      '#e6543b',
-      '#e44226',
-    ];
-    return (
-      <TableRow key={key}>
-        <TableCell
-          align="right"
-          component="th"
-          scope="row"
-          sx={{ backgroundColor: colors[key] }}
-        >
-          {key}
-        </TableCell>
-        {/* Energy */}
-        {formData.category !== 'beverages'
-          ? createTableCell(apiResult.points_a.a, key, notBeveragesEnergy)
-          : createTableCell(apiResult.points_a.a, key, beveragesEnergy)}
-        {/* Sugars */}
-        {formData.category !== 'beverages'
-          ? createTableCell(apiResult.points_a.b, key, notBeveragesSugars)
-          : createTableCell(apiResult.points_a.b, key, beveragesSugars)}
-        {/* Saturated fats/Saturated fats and lipids */}
-        {formData.category !== 'fats'
-          ? key === 0
-            ? createTableCell(apiResult.points_a.c, key, '≤ ' + (key + 1))
-            : createTableCell(apiResult.points_a.c, key, '> ' + key)
-          : createTableCell(apiResult.points_a.c, key, saturatedFatsAndLipids)}
-        {/* Sodium */}
-        {createTableCell(apiResult.points_a.d, key, sodium)}
-      </TableRow>
-    );
-  };
+  ) => (
+    <TableRow key={key}>
+      <TableCell
+        align="right"
+        component="th"
+        scope="row"
+        sx={{ backgroundColor: tableColors.pointsA[key] }}
+      >
+        {key}
+      </TableCell>
+      {/* Energy */}
+      {formData.category !== 'beverages'
+        ? createTableCell(apiResult.points_a.a, key, notBeveragesEnergy)
+        : createTableCell(apiResult.points_a.a, key, beveragesEnergy)}
+      {/* Sugars */}
+      {formData.category !== 'beverages'
+        ? createTableCell(apiResult.points_a.b, key, notBeveragesSugars)
+        : createTableCell(apiResult.points_a.b, key, beveragesSugars)}
+      {/* Saturated fats / Saturated fats and lipids */}
+      {formData.category !== 'fats'
+        ? key === 0
+          ? createTableCell(apiResult.points_a.c, key, '≤ ' + (key + 1))
+          : createTableCell(apiResult.points_a.c, key, '> ' + key)
+        : createTableCell(apiResult.points_a.c, key, saturatedFatsAndLipids)}
+      {/* Sodium */}
+      {createTableCell(apiResult.points_a.d, key, sodium)}
+    </TableRow>
+  );
 
   const createPointsCTableRow = (
     beveragesFruitsAndVegetables,
     fibre,
-    notBeveragesFruitsAndVegetables,
     key,
+    notBeveragesFruitsAndVegetables,
     proteins
-  ) => {
-    const beveragesColors = [
-      '#ffffff',
-      '#e8f2ec',
-      '#d1e5da',
-      '#bad8c7',
-      '#a3cbb5',
-      '#8cbfa2',
-      '#75b290',
-      '#5ea57d',
-      '#47986b',
-      '#308b58',
-      '#197f46',
-    ];
-    const notBeveragesColors = [
-      '#ffffff',
-      '#d1e5da',
-      '#a3cbb5',
-      '#75b290',
-      '#47986b',
-      '#197f46',
-    ];
-    return (
-      <TableRow key={key}>
-        <TableCell
-          align="right"
-          component="th"
-          scope="row"
-          sx={
-            formData.category !== 'beverages'
-              ? key <= 5 && { backgroundColor: notBeveragesColors[key] }
-              : { backgroundColor: beveragesColors[key] }
-          }
-        >
-          {formData.category !== 'beverages' && key > 5 ? '-' : key}
-        </TableCell>
-        {/* Fruits and vegetables */}
-        {formData.category === 'beverages'
-          ? createTableCell(
-              apiResult.points_c.a,
-              key,
-              beveragesFruitsAndVegetables
-            )
-          : createTableCell(
-              apiResult.points_c.a,
-              key,
-              notBeveragesFruitsAndVegetables
-            )}
-        {/* Fibre */}
-        {createTableCell(apiResult.points_c.b, key, fibre)}
-        {/* Proteins */}
-        {createTableCell(apiResult.points_c.c, key, proteins)}
-      </TableRow>
-    );
-  };
-
-  const createTableCell = (comparison, key, text) => {
-    return (
+  ) => (
+    <TableRow key={key}>
       <TableCell
         align="right"
-        sx={{
-          backgroundColor: comparison === key ? 'LightGrey' : '#ffffff',
-        }}
+        component="th"
+        scope="row"
+        sx={
+          // Use different color scale for beverages -> Fruit goes to 10
+          formData.category !== 'beverages'
+            ? key <= 5 && {
+                backgroundColor: tableColors.pointsC.notBeverages[key],
+              }
+            : { backgroundColor: tableColors.pointsC.beverages[key] }
+        }
       >
-        {text}
+        {formData.category !== 'beverages' && key > 5 ? '-' : key}
       </TableCell>
-    );
-  };
+      {/* Fruits and vegetables */}
+      {formData.category === 'beverages'
+        ? createTableCell(
+            apiResult.points_c.a,
+            key,
+            beveragesFruitsAndVegetables
+          )
+        : createTableCell(
+            apiResult.points_c.a,
+            key,
+            notBeveragesFruitsAndVegetables
+          )}
+      {/* Fibre */}
+      {createTableCell(apiResult.points_c.b, key, fibre)}
+      {/* Proteins */}
+      {createTableCell(apiResult.points_c.c, key, proteins)}
+    </TableRow>
+  );
+
+  const createTableCell = (comparison, key, text) => (
+    <TableCell
+      align="right"
+      sx={{
+        backgroundColor: comparison === key ? 'LightGrey' : '#ffffff',
+      }}
+    >
+      {text}
+    </TableCell>
+  );
 
   return (
     apiResult && (
       <div>
         Puntos A y C:
+        {/* Points A table */}
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 400 }} aria-label="Detail table">
             {/* Table headers */}
@@ -254,7 +220,6 @@ const Detail = ({ apiResult, formData }) => {
                 '> 45,0',
                 '> 900'
               )}
-
               {/* Category score */}
               <TableRow key="11" sx={{ border: 2 }}>
                 <TableCell component="th" scope="row"></TableCell>
@@ -266,11 +231,13 @@ const Detail = ({ apiResult, formData }) => {
             </TableBody>
           </Table>
         </TableContainer>
-        Puntos A = a + b + c + d ={' '}
-        {apiResult.points_a.a +
+        {`Puntos A = a + b + c + d = ${
+          apiResult.points_a.a +
           apiResult.points_a.b +
           apiResult.points_a.c +
-          apiResult.points_a.d}
+          apiResult.points_a.d
+        }`}
+        {/* Points C table */}
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 400 }} aria-label="Detail table">
             {/* Table headers */}
@@ -298,21 +265,20 @@ const Detail = ({ apiResult, formData }) => {
               {/* Category score */}
               <TableRow key="11" sx={{ border: 2 }}>
                 <TableCell component="th" scope="row"></TableCell>
-                <TableCell>a = {apiResult.points_a.a}</TableCell>
-                <TableCell>b = {apiResult.points_a.b}</TableCell>
-                <TableCell>c = {apiResult.points_a.c}</TableCell>
+                <TableCell>a = {apiResult.points_c.a}</TableCell>
+                <TableCell>b = {apiResult.points_c.b}</TableCell>
+                <TableCell>c = {apiResult.points_c.c}</TableCell>
               </TableRow>
             </TableBody>
           </Table>
         </TableContainer>
-        Puntos C = a + b + c ={' '}
-        {apiResult.points_a.a + apiResult.points_a.b + apiResult.points_a.c}
-        <br />
+        {`Puntos C = a + b + c = ${
+          apiResult.points_c.a + apiResult.points_c.b + apiResult.points_c.c
+        }`}
         Puntuación final:
+        {/* Final score table */}
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 400 }} aria-label="Detail table">
-            {/* Table headers */}
-            <TableHead></TableHead>
             {/* Table body */}
             <TableBody>
               <TableRow key={0}>
