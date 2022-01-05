9d414b7a8a7524750838403cb902c0d0e1182a0c
Marc Collado <marc@collado.io>
2022-01-05 14:02:16 +0100

    Remove inner container in favor of NutriCard

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Roboto Font
+import { Container } from '@mui/material';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
-// Global container
-import Container from '@mui/material/Container';
 
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -17,11 +14,8 @@ ReactDOM.render(
   <React.StrictMode>
     <Container
       sx={{
-        border: '1px solid #00000025',
-        borderRadius: 2,
         mx: 'auto',
-        my: 2,
-        p: 2,
+        my: 6,
       }}
     >
       <App />
