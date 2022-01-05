95cf232c77b94a1167eebb9a5dfd2973228fa034
Marc Collado <marc@collado.io>
2022-01-05 14:03:03 +0100

    Create NutriCard

---
+++ src/utils/containers.js
@@ -0,0 +1,15 @@
+import { Paper } from '@mui/material';
+
+export const NutriCard = ({ children, ...props }) => (
+  <Paper
+    elevation={4}
+    sx={{
+      // border: '1px solid rgba(0, 0, 0, 0.1)',
+      borderRadius: 4,
+      // boxShadow: '0 7px 30px -10px rgba(150,170,180,0.5)',
+      p: 4,
+    }}
+  >
+    {children}
+  </Paper>
+);
