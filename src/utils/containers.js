b89c6ee4fe68b26df747dcb5f899fdd867e4df67
Marc Collado <marc@collado.io>
2022-01-06 10:32:56 +0100

    Enable custom props

--- src/utils/containers.js
+++ src/utils/containers.js
@@ -1,15 +1,18 @@
 import { Paper } from '@mui/material';
 
-export const NutriCard = ({ children, ...props }) => (
+export const NutriCard = (props) => (
   <Paper
     elevation={4}
     sx={{
       // border: '1px solid rgba(0, 0, 0, 0.1)',
       borderRadius: 4,
       // boxShadow: '0 7px 30px -10px rgba(150,170,180,0.5)',
+      mx: 'auto',
+      my: 3,
       p: 4,
+      ...props.sx,
     }}
   >
-    {children}
+    {props.children}
   </Paper>
 );
