7ae41c89c6a2b2d0c35cd9d03b91a5ba97245d3a
Marc Collado <marc@collado.io>
2022-01-09 09:26:29 +0100

    Remove comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,26 +16,6 @@ const App = () => {
       <Form setApiResult={setApiResult} setFormData={setFormData}></Form>
       {apiResult ? <NutriScore apiResult={apiResult} /> : null}
       {apiResult ? <Detail apiResult={apiResult} formData={formData} /> : null}
-      {/* {apiResult ? (
-        <Box>
-          <h2>Resultado</h2>
-          <li>Final score: {apiResult?.final_score}</li>
-          <li>Nutri score: {apiResult?.nutri_score}</li>
-          <li>Points A:</li>
-          <ul>
-            <li>A: {apiResult?.points_a.a}</li>
-            <li>B: {apiResult?.points_a.b}</li>
-            <li>C: {apiResult?.points_a.c}</li>
-            <li>D: {apiResult?.points_a.d}</li>
-          </ul>
-          <li>Points C:</li>
-          <ul>
-            <li>A: {apiResult?.points_c.a}</li>
-            <li>B: {apiResult?.points_c.b}</li>
-            <li>C: {apiResult?.points_c.c}</li>
-          </ul>
-        </Box>
-      ) : null} */}
     </Box>
   );
 };
