1a56eb5503ae7db72ea722afc91a0dbbe235228f
Marc Collado <marc@collado.io>
2022-02-05 11:49:16 +0100

    Update NutriCard

--- src/utils/containers.js
+++ src/utils/containers.js
@@ -1,18 +1,16 @@
-import { Paper } from '@mui/material';
+import { Box } from '@mui/material';
 
 export const NutriCard = (props) => (
-  <Paper
-    elevation={4}
+  <Box
     sx={{
-      // border: '1px solid rgba(0, 0, 0, 0.1)',
+      border: '1px solid rgba(0, 0, 0, 0.1)',
       borderRadius: 4,
-      // boxShadow: '0 7px 30px -10px rgba(150,170,180,0.5)',
-      mx: 'auto',
-      // my: 3,
-      p: 4,
+      boxShadow: '0 7px 30px -10px rgba(150,170,180,0.5)',
+      p: 5,
+      maxWidth: '1200px',
       ...props.sx,
     }}
   >
     {props.children}
-  </Paper>
+  </Box>
 );
