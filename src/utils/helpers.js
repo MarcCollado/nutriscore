ecb14bce57714238a8e2f18203c24e502fccf89b
Marc Collado <marc@collado.io>
2022-02-09 19:14:44 +0100

    Add NS custom sorting

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,7 @@
 export const capitalize = (word) => {
   return word.charAt(0).toUpperCase() + word.slice(1);
 };
+
+export const nutriScoreSorting = (ns1, ns2) => {
+  return !!ns1 && !!ns2 ? ns1.toUpperCase() - ns2.toUpperCase() : null;
+};
