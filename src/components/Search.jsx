2dab6eeecdb94c3a2f28cd337cc04cd212e982aa
Marc Collado <marc@collado.io>
2022-02-11 18:26:10 +0100

    Use local env when using localhost

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,6 +5,8 @@ import { DataGrid } from '@mui/x-data-grid';
 import SearchIcon from '@mui/icons-material/Search';
 
 import { capitalize } from '../utils/helpers';
+// import { nutriScoreSorting } from '../utils/helpers';
+import seed from '../docs/search.json';
 
 // Base API URL
 const baseApiUrl = `https://nutri-score-app-api.ew.r.appspot.com/`;
@@ -35,15 +37,16 @@ const Search = () => {
   const onSearch = async () => {
     setIsLoading(true);
     // Fetch Search API
-    fetch(`${baseApiUrl}search?q=${JSON.stringify(query)}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        const rows = res.map((r, i) => ({ ...r, id: i }));
-        setIsLoading(false);
-        setApiResult(rows);
-      });
+    const rows =
+      process.env.NODE_ENV === 'development'
+        ? // Use local Database
+          seed.map((r, i) => ({ ...r, id: i }))
+        : // Use OpenFoods Database
+          fetch(`${baseApiUrl}search?q=${JSON.stringify(query)}`)
+            .then((res) => res.json())
+            .then((res) => res.map((r, i) => ({ ...r, id: i })));
+    setIsLoading(false);
+    setApiResult(rows);
   };
 
   return (
@@ -80,6 +83,16 @@ const Search = () => {
             // checkboxSelection
             columns={columns}
             density="compact"
+            initialState={{
+              sorting: {
+                sortModel: [
+                  {
+                    field: 'nutriscore_grade',
+                    sort: 'desc',
+                  },
+                ],
+              },
+            }}
             loading={isLoading}
             onPageSizeChange={(s) => setPageSize(s)}
             pageSize={pageSize}
@@ -94,74 +107,75 @@ const Search = () => {
 };
 
 const columns = [
-  // TODO: Display Letter + order properly
+  // TODO: Order properly
+  // https://mui.com/components/data-grid/sorting/#custom-comparator
   {
     field: 'nutriscore_grade',
     flex: 1,
     headerName: 'Nutri-Score',
     editable: false,
+    // sortComparator: nutriScoreSorting,
+    valueGetter: (params) => capitalize(params.value),
   },
   {
     field: 'name',
     flex: 3,
     headerName: 'Nombre',
-    editable: false,
   },
   {
     field: 'brands',
     flex: 2,
     headerName: 'Brands',
-    editable: false,
     valueGetter: (params) => {
-      params.row.brands.map((b, i) =>
+      return params.row.brands.map((b, i) =>
         i === 0 ? capitalize(b) : ' ' + capitalize(b)
       );
     },
   },
   // TODO: Countries display flags
-  {
-    field: 'countries',
-    flex: 1,
-    headerName: 'Countries',
-    valueGetter: (params) => {
-      // let unique = [...new Set(params.row.countries)];
-      return params.row.countries;
-    },
-  },
+  // {
+  //   field: 'countries',
+  //   flex: 1,
+  //   headerName: 'Countries',
+  //   valueGetter: (params) => {
+  //     // let unique = [...new Set(params.row.countries)];
+  //     return params.row.countries;
+  //   },
+  // },
   {
     field: 'energy',
     flex: 1,
-    headerName: 'Energía',
+    headerName: 'Energía (KJ)',
   },
   {
     field: 'sugars',
     flex: 1,
-    headerName: 'Azúcares',
+    headerName: 'Azúcares (g)',
   },
   {
     field: 'saturated_fats',
     flex: 1,
-    headerName: 'Grasas',
+    headerName: 'Grasas (g)',
   },
   {
     field: 'sodium',
     flex: 1,
-    headerName: 'Sodio',
+    headerName: 'Sodio (g)',
   },
   {
     field: 'fruit_and_vegetables',
     flex: 1,
-    headerName: 'Frutas y vegetales',
+    headerName: 'Frutas y vegetales (%)',
   },
   {
     field: 'fibre',
     flex: 1,
-    headerName: 'Fibra',
+    headerName: 'Fibra (g)',
   },
   {
     field: 'protein',
     flex: 1,
-    headerName: 'Proteínas',
+    headerName: 'Proteínas (g)',
   },
 ];
 
