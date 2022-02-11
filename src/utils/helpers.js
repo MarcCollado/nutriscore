f01d5be626557ec3f1dbec401818c49fe33e2545
Marc Collado <marc@collado.io>
2022-02-11 20:15:48 +0100

    Remove nutriScoreSorting

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,3 @@
 export const capitalize = (word) => {
   return !!word ? word.charAt(0).toUpperCase() + word.slice(1) : word;
 };
-
-export const nutriScoreSorting = (ns1, ns2) => {
-  return !!ns1 && !!ns2 ? ns1.toUpperCase() - ns2.toUpperCase() : null;
-};
