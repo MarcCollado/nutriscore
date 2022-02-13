c027027f423276010ed1d56d0e444a10e56ad68b
Marc Collado <marc@collado.io>
2022-02-13 08:11:00 +0100

    Remove console.log

--- src/components/NutriScore.jsx
+++ src/components/NutriScore.jsx
@@ -4,7 +4,6 @@ import { Box } from '@mui/material';
 import { NutriCard } from '../utils/containers';
 
 const NutriScore = ({ apiResult }) => {
-  console.log(apiResult?.nutriscore_data?.nutri_score.toLowerCase());
   const letter = apiResult?.nutriscore_data?.nutri_score.toLowerCase();
 
   // TODO: highlight color
