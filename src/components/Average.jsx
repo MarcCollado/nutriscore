d319276a5fce8b7d16410d0bf33c4acc20ef48bf
Marc Collado <marc@collado.io>
2022-02-18 19:54:53 +0100

    Add styles depending on values

--- src/components/Average.jsx
+++ src/components/Average.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+
+import { capitalize } from '../utils/helpers';
 
 const Average = ({ apiResult, nutriTarget, property }) => {
+  const currentValue = apiResult.nutriscore_data.points_a[property];
   const similarProducts = apiResult.similar_products.filter(
     (sp) =>
       !!sp.nutriscore_data.points_a[property] &&
@@ -18,14 +21,25 @@ const Average = ({ apiResult, nutriTarget, property }) => {
     !!apiResult.nutriscore_data.nutri_score && (
       <Box
         sx={{
-          border: '1px solid black',
+          border: '1px solid rgba(0, 0, 0, 0.1)',
+          borderRadius: 4,
           mx: 'auto',
+          textAlign: 'center',
         }}
       >
-        {average}
-        {nutriTarget}
-        <br></br>
-        {property}
+        <Typography sx={{ mt: 1, mb: 1 }}>{capitalize(property)}</Typography>
+        <Typography
+          sx={{
+            color: currentValue < average ? 'red' : 'green',
+            fontWeight: 'bold',
+            padding: '2px',
+          }}
+        >
+          {(Math.round(average * 100) / 100).toFixed(2)}
+        </Typography>
+        <Typography sx={{ opacity: 0.5, mb: 1, fontSize: 'smaller' }}>
+          {currentValue}
+        </Typography>
       </Box>
     )
   );
