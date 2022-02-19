7cb24a02d2c127be40bf1f054796d4c1bbb1be96
Marc Collado <marc@collado.io>
2022-02-19 17:18:27 +0100

    Add global theme

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Container } from '@mui/material';
+import { blue } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -13,17 +15,38 @@ import Navbar from './components/Navbar';
 import Search from './components/Search';
 import reportWebVitals from './reportWebVitals';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+    fontSize: 12.5,
+  },
+  palette: {
+    primary: {
+      main: blue[600],
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Container maxWidth="lg">
-        <Routes>
-          <Route path="/" element={<App />}></Route>
-          <Route path="/search" element={<Search />}></Route>
-        </Routes>
-      </Container>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Navbar />
+        <Container maxWidth="lg" sx={{ mb: 8 }}>
+          <Routes>
+            <Route path="/" element={<App />}></Route>
+            <Route path="/search" element={<Search />}></Route>
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
